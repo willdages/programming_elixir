aa77c1595ba19b9473d71ea9d172d42b04852375
Will Dages <wdages@gmail.com>
2015-09-14 21:19:00 -0400

    Add the last exercise from Chapter 6

---
+++ chapter_6/modules-functions-7.exs
@@ -0,0 +1,14 @@
+# Float to string with 2 decimal digits (Erlang)
+:io.format("~.2f", [5.123])
+
+# Get value of an OS environment variable (Elixir)
+System.get_env("USER")
+
+# Return extension of a filename (Elixir)
+Path.extname("dave/test.exs")
+
+# Convert string w/ JSON into Elixir data structures. Just find don't install
+# Answer: Poison library? https://github.com/devinus/poison
+
+# Execute a command in your OS shell
+System.cmd("ls", [])
